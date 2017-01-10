10e157c06cb101ad83bc5ae9b576d9c3da807f32
WanderWang <wangze@egret.com>
2017-01-10 18:14:19 +0800

    fix script error

--- script/src/cli.ts
+++ script/src/cli.ts
@@ -10,11 +10,15 @@ var command = process.argv[2];
 switch (command) {
     case "upgrade":
         var p = getProjectPath(process.argv[3])
-        res.upgrade.run(p).catch(e => console.log (e))
+        res.upgrade.run(p).catch(e => console.log(e))
+        break;
+    case "build":
+        var p = getProjectPath(process.argv[3]);
+        res.build.build(p).catch(e => console.log(e.stack))
         break;
     default:
         var p = getProjectPath(command);
-        res.build.build(p).catch(e => console.log (e.stack))
+        res.build.build(p).catch(e => console.log(e.stack))
         break;
 }
 
