4f637ee86cd532a1e8719e1261987b599dae8433
WanderWang <wangze@egret.com>
2017-01-20 19:03:03 +0800

    add error report

--- script/src/cli.ts
+++ script/src/cli.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import * as res from './';
+import * as fs from 'fs-extra-promise';
+import * as path from 'path';
 
 function getProjectPath(p) {
     return p ? p : ".";
@@ -8,28 +10,32 @@ function getProjectPath(p) {
 
 let handleExceiption = (e: string | Error) => {
     if (typeof e == 'string') {
-        console.log(e);
+        console.log(`错误:${e}`);
     }
     else {
-        console.log(e.stack);
+        console.log(`错误:${e.stack}`);
     }
 }
 
 var command = process.argv[2];
-switch (command) {
-    case "upgrade":
-        var p = getProjectPath(process.argv[3])
-        res.upgrade.run(p).catch(handleExceiption)
-        break;
-    case "build":
-        var p = getProjectPath(process.argv[3]);
-        res.build.build(p).catch(handleExceiption)
-        break;
-    default:
-        var p = getProjectPath(command);
-        res.build.build(p).catch(handleExceiption)
-        break;
+var p = getProjectPath(process.argv[3]);
+if (p && fs.existsSync(path.join(p, "egretProperties.json"))) {
+    switch (command) {
+        case "upgrade":
+            res.upgrade.run(p).catch(handleExceiption)
+            break;
+        case "build":
+            res.build.build(p).catch(handleExceiption)
+            break;
+        default:
+            handleExceiption(`找不到指定的命令{command}`)
+            break;
+    }
 }
+else {
+    handleExceiption(`${path.join(process.cwd(), p)} 不是一个有效的 Egret 项目`)
+}
+
 
 
 
