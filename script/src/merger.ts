eef357cadf907c7a87cf73a7685cfb69389cfc99
WanderWang <185713114@qq.com>
2017-08-08 15:41:54 +0800

    add zip merger

--- script/src/merger.ts
+++ script/src/merger.ts
@@ -4,6 +4,8 @@ import * as utils from 'egret-node-utils';
 import * as fs from 'fs-extra-promise';
 import * as path from 'path';
 
+import * as yazl from 'yazl';
+
 
 let resourcePath;
 let mergeCollection: { [mergeFile: string]: string[] } = {};
@@ -32,18 +34,26 @@ export function walk(f: string) {
     }
 }
 
+
 export function output() {
     for (let mergeFile in mergeCollection) {
         let outputJson = {};
         let sourceFiles = mergeCollection[mergeFile];
         if (ResourceConfig.typeSelector(mergeFile) == "zip") {
-            // sourceFiles.map(sourceFile => {
-            //     let sourcePath = path.join(resourcePath, sourceFile.path);
-            //     let json = fs.readJSONSync(sourcePath);
-            //     outputJson[sourceFile.alias] = json;
-            // })
+
+            var zipfile = new yazl.ZipFile();
+
+            sourceFiles.forEach(sourceFile => {
+                sourceFile = "resource/" + sourceFile;
+                zipfile.addFile(sourceFile, sourceFile);
+            })
+            // pipe() can be called any time after the constructor 
+            zipfile.outputStream.pipe(fs.createWriteStream("output.zip")).on("close", function () {
+                console.log("done");
+            });
+            zipfile.end();
         }
-        fs.writeFileSync(path.join(resourcePath, mergeFile), JSON.stringify(outputJson))
+        // fs.writeFileSync(path.join(resourcePath, mergeFile), JSON.stringify(outputJson))
     }
 
 
